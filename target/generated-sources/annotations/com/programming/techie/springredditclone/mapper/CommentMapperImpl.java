1c326b410dbe7312cffba9701ee4b57e76fd0e23
gerges12 <gergissaaid@gmail.com>
2021-10-06 18:51:55 +0200

    First commit

---
+++ target/generated-sources/annotations/com/programming/techie/springredditclone/mapper/CommentMapperImpl.java
@@ -0,0 +1,57 @@
+package com.programming.techie.springredditclone.mapper;
+
+import com.programming.techie.springredditclone.dto.CommentsDto;
+import com.programming.techie.springredditclone.model.Comment;
+import com.programming.techie.springredditclone.model.Post;
+import com.programming.techie.springredditclone.model.User;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-10-04T18:27:45+0200",
+    comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 1.3.1200.v20200916-0645, environment: Java 15.0.1 (Oracle Corporation)"
+)
+@Component
+public class CommentMapperImpl implements CommentMapper {
+
+    @Override
+    public Comment map(CommentsDto commentsDto, Post post, User user) {
+        if ( commentsDto == null && post == null && user == null ) {
+            return null;
+        }
+
+        Comment comment = new Comment();
+
+        if ( commentsDto != null ) {
+            comment.setText( commentsDto.getText() );
+        }
+        if ( post != null ) {
+            comment.setPost( post );
+        }
+        if ( user != null ) {
+            comment.setUser( user );
+        }
+        comment.setCreatedDate( java.time.Instant.now() );
+
+        return comment;
+    }
+
+    @Override
+    public CommentsDto mapToDto(Comment comment) {
+        if ( comment == null ) {
+            return null;
+        }
+
+        CommentsDto commentsDto = new CommentsDto();
+
+        commentsDto.setCreatedDate( comment.getCreatedDate() );
+        commentsDto.setId( comment.getId() );
+        commentsDto.setText( comment.getText() );
+
+        commentsDto.setUserName( comment.getUser().getUsername() );
+        commentsDto.setPostId( comment.getPost().getPostId() );
+
+        return commentsDto;
+    }
+}
---
+++ target/generated-sources/annotations/com/programming/techie/springredditclone/mapper/SubredditMapperImpl.java
@@ -0,0 +1,49 @@
+package com.programming.techie.springredditclone.mapper;
+
+import com.programming.techie.springredditclone.dto.SubredditDto;
+import com.programming.techie.springredditclone.dto.SubredditDto.SubredditDtoBuilder;
+import com.programming.techie.springredditclone.model.Subreddit;
+import com.programming.techie.springredditclone.model.Subreddit.SubredditBuilder;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-10-04T18:27:45+0200",
+    comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 1.3.1200.v20200916-0645, environment: Java 15.0.1 (Oracle Corporation)"
+)
+@Component
+public class SubredditMapperImpl implements SubredditMapper {
+
+    @Override
+    public SubredditDto mapSubredditToDto(Subreddit subreddit) {
+        if ( subreddit == null ) {
+            return null;
+        }
+
+        SubredditDtoBuilder subredditDto = SubredditDto.builder();
+
+        subredditDto.description( subreddit.getDescription() );
+        subredditDto.id( subreddit.getId() );
+        subredditDto.name( subreddit.getName() );
+
+        subredditDto.numberOfPosts( mapPosts(subreddit.getPosts()) );
+
+        return subredditDto.build();
+    }
+
+    @Override
+    public Subreddit mapDtoToSubreddit(SubredditDto subredditDto) {
+        if ( subredditDto == null ) {
+            return null;
+        }
+
+        SubredditBuilder subreddit = Subreddit.builder();
+
+        subreddit.description( subredditDto.getDescription() );
+        subreddit.id( subredditDto.getId() );
+        subreddit.name( subredditDto.getName() );
+
+        return subreddit.build();
+    }
+}
